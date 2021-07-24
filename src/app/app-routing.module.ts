d348279d0bfd7b80b1bdd835520a70fd562433f2
XDragonSl <almdragonsl@gmail.com>
2021-07-24 16:36:31 +0300

    Added guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { ProductsComponent } from './pages/products/products.component';
 import { BasketComponent } from './pages/basket/basket.component';
 import { OrdersComponent } from './pages/orders/orders.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { AdminGuard } from './shared/guards/admin.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products', component: ProductsComponent },
   { path: 'basket', component: BasketComponent },
-  { path: 'orders', component: OrdersComponent },
+  { path: 'orders', component: OrdersComponent, canActivate: [AdminGuard] },
   { path: '**', component: NotFoundComponent }
 ];
 
