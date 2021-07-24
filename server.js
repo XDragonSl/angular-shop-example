625c70690c6f8abcf77a19a71c840f87c5ab69da
XDragonSl <almdragonsl@gmail.com>
2021-07-24 16:14:22 +0300

    Updated path for compiled app

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(process.cwd() + '/dist/AngularShopExample'));
+app.use(express.static(`${process.cwd()}/dist/angular-shop-example`));
 
 app.get('/*', (req, res) => {
-	res.sendFile(process.cwd() + '/dist/AngularShopExample/index.html');
+	res.sendFile(`${process.cwd()}/dist/angular-shop-example/index.html`);
 });
 
 app.listen(process.env.PORT || 4200);
